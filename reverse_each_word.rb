36a99821e9f5c4e6abae9a37b4b6e9d0bbc1f9fa
Donna Amos <donnacamos.developer@gmail.com>
2019-03-08 01:47:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
-greeting = ["Hello there, and how are you?"]
-def reverse_each_word(greeting)
- greeting.collect do |greeting|
- end 
- greeting.reverse do |greeting|
-   puts greeting
- end 
+
+def reverse_each_word(sentence)
+sentence = 
 end 
